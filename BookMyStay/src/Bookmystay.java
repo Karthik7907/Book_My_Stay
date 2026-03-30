c092ab995e931882def4b3b9695ebe6747bed7cd
Karthik7907 <ck3463@srmist.edu.in>
2026-03-30 15:31:52 +0530

    Booking Cancellation & Inventory Rollback

--- BookMyStay/src/Bookmystay.java
+++ BookMyStay/src/Bookmystay.java
@@ -1,25 +1,15 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-public class Bookmystay
-{
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+public class Bookmystay {
     public static void main(String[] args) {
-        System.out.println("Booking Validation");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Booking Cancellation");
+
         RoomInventory inventory = new RoomInventory();
-        ReservationValidator validator = new ReservationValidator();
-        BookingRequestQueue bookingQueue = new BookingRequestQueue();
-        try {
-            System.out.print("Enter guest name: ");
-            String guestName = scanner.nextLine();
-            System.out.print("Enter room type (Single/Double/Suite): ");
-            String roomType = scanner.nextLine();
-            validator.validate(guestName, roomType);
-            System.out.println("Booking successfully validated and queued!");
-        } catch (InvalidBookingException e) {
-            System.out.println("Booking failed: " + e.getMessage());
-        } finally {
-            scanner.close();
-        }
+        CancellationService cancellationService = new CancellationService();
+
+        cancellationService.cancelBooking("Single-1", "Single", inventory);
+
+        cancellationService.showRollbackHistory();
     }
 }
--- BookMyStay/src/RoomInventory.java
+++ BookMyStay/src/RoomInventory.java
@@ -1,4 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
 
 class RoomInventory {
-    public RoomInventory() {}
+    private Map<String, Integer> inventory = new HashMap<>();
+
+    public RoomInventory() {
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
+    }
+    public void restoreRoom(String roomType) {
+        inventory.put(roomType, inventory.getOrDefault(roomType, 0) + 1);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
 }
