3e9c0641546bc62365afb0a9fa36a5090e6fe6df
Karthik7907 <ck3463@srmist.edu.in>
2026-03-30 14:25:19 +0530

    Room Search & Availability Check

--- BookMyStay/src/Room.java
+++ BookMyStay/src/Room.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+abstract class Room {
+    private int beds;
+    private int size;
+    private double price;
 
-public class Room {
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayInfo() {
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqft");
+        System.out.println("Price per night: " + price);
+    }
 }
